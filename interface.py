cc329a5887cd6a9053602458ba3644f6c1bb240f
ShivSonwanw07 <153204889+ShivSonwanw07@users.noreply.github.com>
2024-04-11 17:15:57 +0530

    initial commit

--- interface.py
+++ interface.py
@@ -1,20 +1,64 @@
 
+# import streamlit as st
+# import pandas as pd 
+# import pickle
+# import string 
+# import spacy
+# from sklearn.feature_extraction.text import TfidfVectorizer
+
+# st.title('Text Classification Model to Identify Active or Passive Voice Statement')
+# labels = {'Active': 0, 'Passive': 1}
+# nlp = spacy.load("en_core_web_sm")
+
+# # Load the TfidfVectorizer used during training
+# tfidf_vectorizer = pickle.load(open('tfidf_vectorizer.pkl', 'rb'))
+
+# def preprocess_text(text):
+#     return " ".join([token.lemma_ for token in nlp(text)])
+
+# punc = string.punctuation
+# model = pickle.load(open('Active_Passive.pkl', 'rb'))
+
+# def predict(statement):
+#     statement = statement.lower()
+#     statement = "".join(i for i in statement if i not in punc)
+#     processed_statement = preprocess_text(statement)
+
+#     tfidf_features = tfidf_vectorizer.transform([processed_statement])
+#     logi_pred = model.predict(tfidf_features)
+#     for key, value in labels.items():
+#         if value == logi_pred[0]:
+#             return key
+        
+# statement = st.text_input('Enter the Statement:-')
+
+# if st.button('Identify!'):
+#     a = predict(statement)
+#     st.header(f"The given Statement is in {a} Voice")
 import streamlit as st
 import pandas as pd 
 import pickle
 import string 
-import spacy
+import nltk
 from sklearn.feature_extraction.text import TfidfVectorizer
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+# Download necessary NLTK resources
+nltk.download('punkt')
+nltk.download('wordnet')
 
 st.title('Text Classification Model to Identify Active or Passive Voice Statement')
 labels = {'Active': 0, 'Passive': 1}
-nlp = spacy.load("en_core_web_sm")
 
 # Load the TfidfVectorizer used during training
 tfidf_vectorizer = pickle.load(open('tfidf_vectorizer.pkl', 'rb'))
 
 def preprocess_text(text):
-    return " ".join([token.lemma_ for token in nlp(text)])
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+    tokens = [lemmatizer.lemmatize(token.lower()) for token in tokens if token.isalnum()]
+    return " ".join(tokens)
 
 punc = string.punctuation
 model = pickle.load(open('Active_Passive.pkl', 'rb'))
